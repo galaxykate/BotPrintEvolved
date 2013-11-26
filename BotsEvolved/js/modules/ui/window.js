1d4fbcbea7f9f71f937cc5cc1e4bf8e16f70eb67
Kate <galaxykate@gmail.com>
2013-11-25 18:58:24 -0800

    Adding useful window file

---
+++ BotsEvolved/js/modules/ui/window.js
@@ -0,0 +1,57 @@
+/**
+ * @author Kate Compton
+ */
+
+define(["common"], function(common) {'use strict';
+
+    var DrawingWindow = Class.extend({
+        init : function(name, element) {
+            this.name = name;
+            this.w = element.width();
+            this.h = element.height();
+            this.element = element;
+            this.active = false;
+            this.transform = new common.Transform();
+            this.localPos = new Vector();
+        },
+
+        setProcessing : function(processing) {
+            this.processing = processing;
+            this.transform.translation.setTo(processing.width / 2, processing.height / 2);
+
+        },
+
+        getPositionRelativeTo : function(element, pos) {
+            var v = new Vector(pos.x - element.offset().left, pos.y - element.offset().top);
+            return v;
+        },
+
+        setLocalPosition : function(mousePos) {
+            var p = app.getPositionRelativeTo(this.element, mousePos);
+            this.transform.toLocal(p, this.localPos);
+        },
+
+        render : function(toRender) {
+            var g = this.processing;
+            var context = {
+                g : g
+            };
+
+            g.colorMode(g.HSB, 1);
+            g.background(.75, .1, 1);
+
+            g.pushMatrix();
+            this.transform.applyTransform(g);
+            toRender(context);
+
+            g.fill(0);
+            g.ellipse(this.localPos.x, this.localPos.y, 20, 20);
+            g.popMatrix();
+        },
+
+        toString : function() {
+            return "Window " + this.name + "(" + this.active + ")";
+        }
+    });
+    return DrawingWindow;
+});
