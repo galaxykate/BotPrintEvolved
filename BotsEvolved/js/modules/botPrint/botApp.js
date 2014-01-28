f2e8431cc37c91cfc4fab8ebed0cf41486c86fc4
Kate <galaxykate@gmail.com>
2014-01-27 16:15:30 -0800

    Got interface to switch between edit and arena modes, added pause ability

--- BotsEvolved/js/modules/botPrint/botApp.js
+++ BotsEvolved/js/modules/botPrint/botApp.js
@@ -7,23 +7,66 @@ define(["ui", "./bot/bot", "./physics/arena", "threeUtils", "./botEvo", "app", "
     var BotApp = App.extend({
         init : function() {
             var app = this;
+            app.paused = false;
 
-            var w = 800;
-            var h = 600;
-            this.fullPanelDimensions = new Vector(w - 20, h - 40);
-
-            app._super("Bots", new Vector(w, h));
+            app._super("Bots", new Vector(30, 30));
 
             // app.changeMode("inspector");
-            app.editBot();
             app.arena = new Arena();
 
-            app.changeMode("arena");
+            app.currentBot = new Bot();
 
-            //   this.createAndTestNewBot();
-            this.createAndTestManyBots();
+            $("#switch_modes").click(function() {
+                app.toggleMainMode();
+            });
+
+            app.openEditMode();
+            app.closeLoadScreen();
+        },
+
+        //=====================================================================
+        //=====================================================================
+        //=====================================================================
+        //=====================================================================
+
+        toggleMainMode : function() {
+            console.log("Toggle main mode " + app.editMode);
+            if (app.editMode)
+                app.openArenaMode();
+            else
+                app.openEditMode();
+        },
+
+        openEditMode : function() {
+            app.editMode = true;
+            $("#arena").addClass("away");
+            $("#edit").removeClass("away");
+
+            // Make wiring for this bot?
+            app.currentBot.transform.setTo(0, 0, 0);
 
         },
+
+        openArenaMode : function() {
+            app.editMode = false;
+            $("#edit").addClass("away");
+            $("#arena").removeClass("away");
+            this.createAndTestManyBots();
+        },
+
+        openLoadScreen : function() {
+            $("#load_screen").show();
+        },
+
+        closeLoadScreen : function() {
+            $("#load_screen").hide();
+
+        },
+
+        //=====================================================================
+        //=====================================================================
+        //=====================================================================
+        //=====================================================================
 
         createAndTestManyBots : function() {
             var task = "doThing";
@@ -61,119 +104,31 @@ define(["ui", "./bot/bot", "./physics/arena", "threeUtils", "./botEvo", "app", "
             app.ui.addOption("logConditionTests", false);
             app.ui.addOption("logMutations", true);
             app.ui.addOption("useTimers", true);
-            app.ui.addOption("useSharpie", true);
-
-            ui.addPanel({
-                id : "arena",
-                div : $("#arena_panel"),
-                title : "Arena",
-                description : "An arena to view bots in",
-                side : "right",
-                sidePos : 5,
-                dimensions : app.fullPanelDimensions
-
-            });
-
-            ui.addPanel({
-                id : "inspector",
-                div : $("#inspector_panel"),
-                title : "Inspector",
-                description : "A window to inspect and modify bots in",
-                side : "left",
-                sidePos : 5,
-                dimensions : app.fullPanelDimensions
-
-            });
-
-            ui.addPanel({
-                id : "threeRender",
-                div : $("#render_panel"),
-                title : "Render Panel",
-                description : "3D preview",
-                side : "top",
-                sidePos : 5,
-                dimensions : new Vector(200, 200),
-
-            });
-
-            ui.addPanel({
-                id : "scores",
-                div : $("#scores_panel"),
-                title : "Scores Panel",
-                description : "Current scores",
-                side : "top",
-                sidePos : 5,
-                dimensions : new Vector(400, 100),
-
-            });
-
-            // Create modes:
-            // Each mode has some panels that only appear during that mode, and
-            // Some custom control functionality
-
-            this.modes = {
-                arena : new UI.Mode({
-                    title : "arena",
-                    panels : app.ui.getPanels(["arena", "scores"]),
-
-                }),
-
-                inspector : new UI.Mode({
-                    title : "inspector",
-                    panels : app.ui.getPanels(["inspector", "threeRender"]),
-
-                }),
-
-            };
-
-            $.each(this.modes, function(key, mode) {
-                mode.id = key;
-            });
+            app.ui.addOption("useSharpie", false);
 
         },
-
         initControls : function() {
 
             // Set all the default UI controls
             app.controls = new UI.Controls($("body"), {
 
                 onKeyPress : {
+
                     d : function(event) {
                         app.ui.devMode.toggle()
                     },
+
+                    space : function() {
+                        app.paused = !app.paused;
+                    }
                 },
 
             });
 
             // Make some of the windows touchable
 
-            var touchRender = app.controls.addTouchable("threeRender", $("#render_panel"));
-            var touchInspector = app.controls.addTouchable("inspector", $("#inspector_canvas"));
+            var touchInspector = app.controls.addTouchable("inspector", $("#edit_canvas"));
             var touchArena = app.controls.addTouchable("arena", $("#arena_canvas"));
-
-            // Camera Controls
-            touchRender.onDrag(function(touchwindow, p) {
-                var cam = app.threeRender.camera;
-                cam.orbit.theta = p.x * -.003;
-                cam.orbit.phi = -p.y * .004 + 1.7;
-                cam.updateOrbit();
-
-            });
-            touchRender.onUp(function(touchwindow, p) {
-                app.pauseSpinning = false;
-            });
-
-            touchRender.onDown(function(touchwindow, p) {
-                app.pauseSpinning = true;
-            });
-
-            touchRender.onScroll(function(touchwindow, delta) {
-                var cam = app.threeRender.camera;
-                cam.orbit.distance *= 1 + .03 * delta;
-                cam.orbit.distance = utilities.constrain(cam.orbit.distance, 300, 1200);
-                cam.updateOrbit();
-
-            });
 
             // Inspector controls
             touchInspector.onDrag(function(touchwindow, p) {
@@ -191,62 +146,8 @@ define(["ui", "./bot/bot", "./physics/arena", "threeUtils", "./botEvo", "app", "
             });
 
         },
-
         initUI : function() {
-
-            $("#test_button").click(function() {
-                app.changeMode("arena");
-            });
-
-            $("#reset_arena").click(function() {
-                app.createAndTestNewBot();
-            });
-
-            $("#mutate").click(function() {
-                console.log("-------------------------- ");
-                console.log("Mutating ");
-                app.currentBot.brain.defaultTree.debugPrint();
-                app.evoSim.mutateGenome(app.currentBot.brain.defaultTree);
-
-                app.evoSim.treeViz.setTree(app.currentBot.brain.defaultTree);
-                console.log("defaultTree after: ", app.currentBot.brain.defaultTree)
-
-            });
-
-            $("#mutateBig").click(function() {
-                console.log("-------------------------- ");
-                console.log("Mutating Generations");
-                app.evoSim.runGenerations(2);
-                /*
-                app.currentBot.brain.defaultTree.debugPrint();
-                for (var i = 0; i < 20; i++) {
-                    app.evoSim.mutateGenome(app.currentBot.brain.defaultTree);
-                }
-
-                app.evoSim.treeViz.setTree(app.currentBot.brain.defaultTree);
-                app.currentBot.brain.defaultTree.debugPrint();
-                */
-            });
-
-            $("#spawnRelatives").click(function() {
-                console.log("Spawn relatives");
-                var bots = [];
-                // Create relatives
-                console.log(app.currentBot.clone());
-                for (var i = 0; i < 10; i++) {
-                    bots[i] = app.currentBot.clone();
-                    // bots[i].mutate(.7);
-                }
-                app.arena.addPopulation(bots);
-            });
-
-            var ui = this.ui;
-
-            $(".full_canvas").css({
-                width : "800px",
-                height : "600px",
-            });
-
+            var ui = app.ui;
             // Create the Three scene
             app.threeRender = new threeUtils.ThreeView($("#render_panel"), function() {
                 // update the camera
@@ -273,26 +174,36 @@ define(["ui", "./bot/bot", "./physics/arena", "threeUtils", "./botEvo", "app", "
             app.threeRender.scene.add(app.threeBotMesh);
 
             // These windows all use processing for the drawing
-            app.inspectorWindow = new UI.DrawingWindow("inspector", $("#inspector_canvas"));
+            app.editWindow = new UI.DrawingWindow("edit", $("#edit_canvas"));
             app.arenaWindow = new UI.DrawingWindow("arena", $("#arena_canvas"));
-            app.scoreWindow = new UI.DrawingWindow("score", $("#score_canvas"));
 
-            app.inspectorProcessing = ui.addProcessingWindow(app.inspectorWindow.element, function(g) {
-                app.inspectorWindow.setProcessing(g);
+            app.editorProcessing = ui.addProcessingWindow(app.editWindow.element, function(g) {
+                app.editWindow.setProcessing(g);
 
             }, function(g) {
                 // Updates
 
                 // only do if its the inspector mode
-                if (app.mode === app.modes.inspector) {
+                if (app.editMode) {
                     // Updates
 
-                    app.worldTime.updateTime(g.millis() * .001);
-                    app.currentBot.update(app.time);
+                    if (!app.paused) {
+                        app.worldTime.updateTime(g.millis() * .001);
+                        app.currentBot.update(app.worldTime.ellapsed);
+                    }
 
-                    app.inspectorWindow.render(function(context) {
+                    app.editWindow.render(function(context) {
+
+                        var g = context.g;
+                        g.background(.8);
                         context.useChassisCurves = true;
+                        // Draw the bot
+
+                        g.fill(.8, 1, 1);
+                        g.ellipse(0, 0, 90, 90);
+
                         app.currentBot.render(context);
+
                     });
                 }
             });
@@ -301,44 +212,20 @@ define(["ui", "./bot/bot", "./physics/arena", "threeUtils", "./botEvo", "app", "
             app.arenaProcessing = ui.addProcessingWindow(app.arenaWindow.element, function(g) {
                 app.arenaWindow.setProcessing(g);
             }, function(g) {
-
                 // only do if its the arena mode
-                if (app.mode === app.modes.arena) {
-                    app.worldTime.updateTime(g.millis() * .001);
+                if (!app.editMode) {
 
-                    app.arena.update(app.worldTime.ellapsed);
-
+                    if (!app.paused) {
+                        app.worldTime.updateTime(g.millis() * .001);
+                        app.arena.update(app.worldTime.ellapsed);
+                    }
                     app.arenaWindow.render(function(context) {
+                        context.scale = 3;
                         app.arena.render(context);
                     });
                 }
 
             });
-
-            // Create the arena
-            app.scoreProcessing = ui.addProcessingWindow(app.scoreWindow.element, function(g) {
-                app.scoreWindow.setProcessing(g);
-            }, function(g) {
-
-                // only do if its the arena mode
-                if (app.mode === app.modes.arena && app.evoSim) {
-                    g.background(.8, 1, .3);
-                    app.evoSim.renderScores(g);
-
-                }
-
-            });
-
-        },
-
-        editBot : function(bot) {
-            app.changeMode("inspector");
-
-            if (bot === undefined)
-                bot = new Bot();
-
-            app.currentBot = bot;
-            app.threeBotMesh.add(bot.createThreeMesh());
 
         },
     });
