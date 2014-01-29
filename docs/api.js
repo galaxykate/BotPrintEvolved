f899d20e43291e1eb71e42468861374e22d50402
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2014-01-29 15:34:47 -0800

    Added docs

---
+++ docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "App",
+        "BrainEvo",
+        "Collision",
+        "Coords",
+        "Draggable",
+        "EvoSim",
+        "Gridster",
+        "Time"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
